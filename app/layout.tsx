757ac58841c8c5350400e1f3b5db6c6d9790fdde
Trevor Keith <tk@tkeith.com>
2023-05-13 15:15:48 +0100

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { GlobalStateProvider } from './page'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { GlobalStateProvider } from "./page";
 
 export const metadata = {
   title: "Credential Corgi",
   description: "Created by the Corgi Team",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <GlobalStateProvider>
-      {/* <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html> */}
-
       <html lang="en">
-        <body className='bg-white'>{children}</body>
+        <body className={"bg-white"}>{children}</body>
       </html>
-
-      {/* <html lang="en" className='h-full bg-white'>
-    <body className='h-full'>{children}</body>
-    </html> */}
     </GlobalStateProvider>
-  )
+  );
 }
