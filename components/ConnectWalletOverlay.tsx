e348d6aa2376976168d16e410de0adb60ac7699a
Trevor Keith <tk@tkeith.com>
2023-05-14 07:50:19 +0100

    styling

--- components/ConnectWalletOverlay.tsx
+++ components/ConnectWalletOverlay.tsx
@@ -5,7 +5,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export const ConnectWalletOverlay: React.FC = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10">
       <div className="flex items-center p-4 bg-white border-2 border-corgi rounded-xl w-96 h-48 p-8">
         <Image
           src="/corgi-logo.png"
